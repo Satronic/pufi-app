8ad0859f6701e38655064e39c9514eb2945e5392
Sneider <electronsgm@gmail.com>
2022-09-29 10:24:49 -0500

    Structure of the header and the navigation bar

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import Navbar from '../navbar/Navbar.js'
+
+function Header() {
+    return (
+    <header className="header">
+        <Navbar />
+    </header>
+);
+}
+
+export default Header;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,50 @@
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+import logo_rain from '../../assets/logo-rain.svg';
+import {NavLink} from 'react-router-dom';
+
+function Navbar() {
+    return (
+    <nav className="navbar">
+        <div className="navbar-container-logo">
+            <img src={logo} alt="logo"></img>
+        </div>
+        <div className="navbar-container-menu">
+            
+            <NavLink className="navbar-link">
+                <img className="logo-rain" src={logo_rain} alt="logo-rain" />
+                PUFI PUFF
+            </NavLink>
+        
+        
+            <NavLink className="navbar-link">
+                <img className="logo-rain" src={logo_rain} alt="logo-rain" />
+                PUFI RAIN
+            </NavLink>
+    
+        
+            <NavLink className="navbar-link">
+                <img className="logo-rain" src={logo_rain} alt="logo-rain" />
+                PUFI CART
+            </NavLink>
+    
+        
+            <NavLink className="navbar-link">
+                <img className="logo-rain" src={logo_rain} alt="logo-rain" />
+                PUFI NAP
+            </NavLink>
+
+        </div>
+        <div className="navbar-container-acount">
+            <NavLink className="navbar-link">
+                MI CUENTA
+            </NavLink>
+            <NavLink className="navbar-link">
+                MI COMPRA
+            </NavLink>
+        </div>
+    </nav>
+);
+}
+
+export default Navbar;
