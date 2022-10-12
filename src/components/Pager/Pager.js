f6b9a91f986dc006cdce082a883ec39730b1b176
Sneider <electronsgm@gmail.com>
2022-10-12 02:07:43 -0500

    Fix errors in pager component

--- src/components/Pager/Pager.js
+++ src/components/Pager/Pager.js
@@ -1,22 +1,26 @@
 import './Pager.css';
 import { createPages } from '../../utils/index.js';
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changePage } from '../../Redux/Actions/index.js';
 
 
 function Pager({products}) {
+    
+    const dispatch = useDispatch();
+    const valuePages = useSelector(state => state.pages);
+    console.log(valuePages);
+
+    const [statePages, setStatePages] = useState({
+        activePage: '1',
+        minLimit: 0,
+        maxLimit: 15
+    });
+    
     const productsByPage = 15;
     const pages = createPages(products.length, productsByPage);
     const numberPages = Object.keys(pages);
 
-    const dispatch = useDispatch();
-    const [statePages, setStatePages] = useState({
-        activePage: '1',
-        minLimit: 1,
-        maxLimit: 15
-    });
-    
     useEffect(() => {
         dispatch(changePage([statePages.minLimit, statePages.maxLimit]));
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -35,7 +39,7 @@ function Pager({products}) {
     
     return (
         <div className="pager">
-            <p className="pager-title">{`Productos ${statePages.minLimit + 1} al ${statePages.maxLimit} de ${products.length}`}</p>
+            <p className="pager-title">{`Productos ${valuePages[0] + 1} al ${valuePages[1]} de ${products.length}`}</p>
             {numberPages.map(page => {
                 return <button className={statePages.activePage === page ? 'page-active' : 'pager-number-page'} 
                     value={page} 
